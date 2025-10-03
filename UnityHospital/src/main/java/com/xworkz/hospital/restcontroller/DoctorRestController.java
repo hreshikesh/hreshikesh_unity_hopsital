acfcc5831824c6c77c9f4a2fa551daffa87ac2e2
Hreshikesh <ailhreshikesh@gmail.com>
2025-10-03 11:53:58 +0530

    Unity Hospital

--- UnityHospital/src/main/java/com/xworkz/hospital/restcontroller/DoctorRestController.java
+++ UnityHospital/src/main/java/com/xworkz/hospital/restcontroller/DoctorRestController.java
@@ -4,9 +4,11 @@ import com.xworkz.hospital.service.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class DoctorRestController {
     @Autowired
     HospitalService service;
--- UnityHospital/src/main/java/com/xworkz/hospital/restcontroller/SlotRestController.java
+++ UnityHospital/src/main/java/com/xworkz/hospital/restcontroller/SlotRestController.java
@@ -8,15 +8,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 @RestController
 @Slf4j
+@RequestMapping("/")
 public class SlotRestController {
 
     @Autowired
@@ -24,7 +22,7 @@ public class SlotRestController {
 
 
     @Autowired
-    PatientService patientService;.
+    PatientService patientService;
 
 
     @GetMapping("checkInterval/{specialization}/{timeInterval}")
